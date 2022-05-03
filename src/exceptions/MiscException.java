31a1b7ea919745be2b326e46ad90760098828551
Youssef Fahmy <youssef.tfs2@gmail.com>
2022-05-04 00:14:11 +0200

    Added MiscException

---
+++ src/exceptions/MiscException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class MiscException extends RuntimeException {
+
+	public MiscException() {
+		super();
+	}
+
+	public MiscException(String message) {
+		super(message);
+	}
+
+}
